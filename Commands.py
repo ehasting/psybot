863c817c9bd738ccd65395b068f3433e96d1b69e
Egil Hasting[gui] <eh@synergysky.com>
2016-03-28 21:02:46 +0200

    time zone feat

--- Commands.py
+++ Commands.py
@@ -153,12 +153,12 @@ class Stats(GeneralMessageEvent):
         yield from self.bot.sendMessage("{}".format(output_string), parse_mode="HTML")
 
     def sort_by_word(self, userdict):
-        Loggiz.L.info(str(userdict))
-        usercountobject = SerializableDict.UserObject(userdict)
-        if not isinstance(usercountobject.wordcounter, int):
-            return 1
-        Loggiz.L.info(usercountobject.wordcounter)
-        return usercountobject.wordcounter
+        for key, value in userdict:
+            usercountobject = SerializableDict.UserObject(value)
+            if not isinstance(usercountobject.wordcounter, int):
+                return 1
+            Loggiz.L.info(usercountobject.wordcounter)
+            return usercountobject.wordcounter
 
 
 class Help(GeneralMessageEvent):
